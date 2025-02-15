ed74c68a89d9f498ef95d99c3eb76e4e0f18e936
Matheus Bustamante <mtbustamante98@gmail.com>
2025-02-15 10:41:57 -0300

    finalizando projeto, melhorias no código serão feitas

--- js/script.js
+++ js/script.js
@@ -29,44 +29,81 @@ function updateStep() {
 
 function nextStep(event) {
   if (event) event.preventDefault();
-  if (!inputName.checkValidity() || !inputEmail.checkValidity()) {
-    alert("Please fill in all fields correctly.");
-    return;
+  // if (!inputName.checkValidity() || !inputEmail.checkValidity()) {
+  //   alert("Please fill in all fields correctly.");
+  //   return;
+  // }
+
+  if (currentStep === 1) {
+    const selectedTopics = getSelectedTopics();
+    if (selectedTopics.length === 0) {
+      alert("Please select at least one item from the list!");
+      return;
+    }
   }
   if (currentStep < stepContainers.length - 1) currentStep++;
-
   updateStep();
-  checkValue();
-  listLIchecked();
+  // checkValue();
 }
 
-function checkValue() {
-  const valueName = inputName.value;
-  const valueEmail = inputEmail.value;
+// function checkValue() {
+//   const valueName = inputName.value;
+//   const valueEmail = inputEmail.value;
 
-  spanValueName.innerText = valueName;
-  spanValueEmail.innerText = valueEmail;
-}
+//   spanValueName.innerText = valueName;
+//   spanValueEmail.innerText = valueEmail;
+// }
 
 function listLIchecked() {
   let listLIarray = [...listLI];
   let arrayInformation = [...dataInformation];
   let selectedTopics = [];
   listLIarray.forEach((element) => {
-    element.addEventListener("click", (event) => {
+    element.addEventListener("click", () => {
       const checkbox = element.querySelector("input[type='checkbox']");
       checkbox.checked = !checkbox.checked;
-
 
       selectedTopics = listLIarray
         .filter((li) => li.querySelector("input[type='checkbox']").checked)
         .map((li) => li.querySelector("label").innerText);
 
       arrayInformation.forEach((element, index) => {
-        element.innerText = selectedTopics[index] || "";
+        const topicsListContainer = document.querySelector(".list-topics");
+        const topicsList = topicsListContainer.querySelector("ul");
+        if (!topicsList) return;
+
+        topicsList.innerHTML = "";
+
+        selectedTopics.forEach((topic) => {
+          const li = document.createElement("li");
+          li.innerText = topic;
+          topicsList.appendChild(li);
+          topicsListContainer.classList.add("ativo");
+        });
+
+        if (selectedTopics.length === 1) {
+          if (topicsListContainer.length > 1) topicsListContainer[1].style.visibility = "hidden";
+          if (topicsListContainer.length > 2) topicsListContainer[2].style.visibility = "hidden";
+          element.innerText = selectedTopics[index];
+        } else if (selectedTopics.length === 2) {
+          if (topicsListContainer.length > 1) topicsListContainer[1].style.visibility = "visible";
+          if (topicsListContainer.length > 2) topicsListContainer[2].style.visibility = "hidden";
+          element.innerText = selectedTopics[index];
+        } else {
+          if (topicsListContainer.length > 1) topicsListContainer[1].style.visibility = "visible";
+          if (topicsListContainer.length > 2) topicsListContainer[2].style.visibility = "visible";
+          element.innerText = selectedTopics[index];
+        }
       });
     });
   });
+}
+listLIchecked();
+
+function getSelectedTopics() {
+  return [
+    ...document.querySelectorAll(".list li input[type='checkbox']:checked"),
+  ].map((checkbox) => checkbox.closest("li").querySelector("label").innerText);
 }
 
 function stepSucess() {
