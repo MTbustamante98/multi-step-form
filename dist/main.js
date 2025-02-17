93519f47d77011de23a14504d8afb09b864cfef1
Matheus Bustamante <139372893+MTbustamante98@users.noreply.github.com>
2025-02-16 21:58:10 -0300

    projeto finalizado!

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,146 @@
-(()=>{"use strict";function e(e,t){(null==t||t>e.length)&&(t=e.length);for(var n=0,r=Array(t);n<t;n++)r[n]=e[n];return r}function t(t){return function(t){if(Array.isArray(t))return e(t)}(t)||function(e){if("undefined"!=typeof Symbol&&null!=e[Symbol.iterator]||null!=e["@@iterator"])return Array.from(e)}(t)||function(t,n){if(t){if("string"==typeof t)return e(t,n);var r={}.toString.call(t).slice(8,-1);return"Object"===r&&t.constructor&&(r=t.constructor.name),"Map"===r||"Set"===r?Array.from(t):"Arguments"===r||/^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(r)?e(t,n):void 0}}(t)||function(){throw new TypeError("Invalid attempt to spread non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.")}()}var n,r,o,l=document.querySelectorAll(".first-content, .second-content, .third-content"),i=document.getElementById("name"),c=document.getElementById("email"),a=document.querySelectorAll(".list li"),u=document.querySelector("[data-value-name]"),d=document.querySelector("[data-value-email]"),s=document.querySelectorAll("[data-information]"),f=document.querySelectorAll(".btn"),y=document.getElementById("last-button-submit"),m=["click","touchstart"],h=0;function v(e){if(e&&e.preventDefault(),i.checkValidity()&&c.checkValidity()){var n,r;if(1===h&&0===t(document.querySelectorAll(".list li input[type='checkbox']:checked")).map((function(e){return e.closest("li").querySelector("label").innerText})).length)return void alert("Please select at least one item from the list!");h<l.length-1&&h++,l.forEach((function(e,t){e.style.display=t===h?"block":"none"})),n=i.value,r=c.value,u.innerText=n,d.innerText=r}else alert("Please fill in all fields correctly.")}function b(){alert("Sucess"),setTimeout((function(){location.reload()}),100)}n=t(a),r=t(s),o=[],n.forEach((function(e){e.addEventListener("click",(function(){var t=e.querySelector("input[type='checkbox']");t.checked=!t.checked,o=n.filter((function(e){return e.querySelector("input[type='checkbox']").checked})).map((function(e){return e.querySelector("label").innerText}));var l=document.querySelector(".list-topics"),i=l.querySelector("ul");i&&(i.innerHTML="",o.forEach((function(e){var t=document.createElement("li");t.innerText=e,i.appendChild(t)})),l.classList.toggle("ativo",o.length>0),r.forEach((function(e,t){t<o.length?e.innerText=o[t]:e.innerText=""})),i.querySelectorAll("li").forEach((function(e,t){e.style.visibility=t<o.length?"visible":"hidden"})))}))})),Array.isArray(m)&&m.length>0?m.forEach((function(e){f.forEach((function(t){t.addEventListener(e,v)})),"submit"===e?y.addEventListener("submit",(function(e){b()})):y.addEventListener(e,b)})):console.error("Error: The 'events' array is either empty or not defined.")})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./js/script.js":
+/*!**********************!*\
+  !*** ./js/script.js ***!
+  \**********************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _babel_runtime_helpers_toConsumableArray__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! @babel/runtime/helpers/toConsumableArray */ \"./node_modules/@babel/runtime/helpers/esm/toConsumableArray.js\");\n\n//steps\nvar stepContainers = document.querySelectorAll(\".first-content, .second-content, .third-content\");\n\n//Input's\nvar inputName = document.getElementById(\"name\");\nvar inputEmail = document.getElementById(\"email\");\nvar listLI = document.querySelectorAll(\".list li\");\n\n//Third-content elements\nvar spanValueName = document.querySelector(\"[data-value-name]\");\nvar spanValueEmail = document.querySelector(\"[data-value-email]\");\nvar dataInformation = document.querySelectorAll(\"[data-information]\");\n\n//Button\nvar btns = document.querySelectorAll(\".btn\");\nvar btnSubmit = document.getElementById(\"last-button-submit\");\n\n//events\nvar events = [\"click\", \"touchstart\"];\nvar currentStep = 0;\nfunction updateStep() {\n  stepContainers.forEach(function (steps, index) {\n    steps.style.display = index === currentStep ? \"block\" : \"none\";\n  });\n}\nfunction nextStep(event) {\n  if (event) event.preventDefault();\n  if (!inputName.checkValidity() || !inputEmail.checkValidity()) {\n    alert(\"Please fill in all fields correctly.\");\n    return;\n  }\n  if (currentStep === 1) {\n    var selectedTopics = getSelectedTopics();\n    if (selectedTopics.length === 0) {\n      alert(\"Please select at least one item from the list!\");\n      return;\n    }\n  }\n  if (currentStep < stepContainers.length - 1) currentStep++;\n  updateStep();\n  checkValue();\n}\nfunction checkValue() {\n  var valueName = inputName.value;\n  var valueEmail = inputEmail.value;\n  spanValueName.innerText = valueName;\n  spanValueEmail.innerText = valueEmail;\n}\nfunction listLIchecked() {\n  var listLIarray = (0,_babel_runtime_helpers_toConsumableArray__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(listLI);\n  var arrayInformation = (0,_babel_runtime_helpers_toConsumableArray__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(dataInformation);\n  var selectedTopics = [];\n  listLIarray.forEach(function (element) {\n    element.addEventListener(\"click\", function () {\n      var checkbox = element.querySelector(\"input[type='checkbox']\");\n      checkbox.checked = !checkbox.checked;\n      selectedTopics = listLIarray.filter(function (li) {\n        return li.querySelector(\"input[type='checkbox']\").checked;\n      }).map(function (li) {\n        return li.querySelector(\"label\").innerText;\n      });\n      var topicsListContainer = document.querySelector(\".list-topics\");\n      var topicsList = topicsListContainer.querySelector(\"ul\");\n      if (!topicsList) return;\n      topicsList.innerHTML = \"\";\n      selectedTopics.forEach(function (topic) {\n        var li = document.createElement(\"li\");\n        li.innerText = topic;\n        topicsList.appendChild(li);\n      });\n      topicsListContainer.classList.toggle(\"ativo\", selectedTopics.length > 0);\n      arrayInformation.forEach(function (element, index) {\n        if (index < selectedTopics.length) {\n          element.innerText = selectedTopics[index];\n        } else {\n          element.innerText = \"\";\n        }\n      });\n      var topicItems = topicsList.querySelectorAll(\"li\");\n      topicItems.forEach(function (li, index) {\n        li.style.visibility = index < selectedTopics.length ? \"visible\" : \"hidden\";\n      });\n    });\n  });\n}\nlistLIchecked();\nfunction getSelectedTopics() {\n  return (0,_babel_runtime_helpers_toConsumableArray__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(document.querySelectorAll(\".list li input[type='checkbox']:checked\")).map(function (checkbox) {\n    return checkbox.closest(\"li\").querySelector(\"label\").innerText;\n  });\n}\nfunction stepSucess() {\n  alert(\"Sucess\");\n  setTimeout(function () {\n    location.reload();\n  }, 100);\n}\nif (Array.isArray(events) && events.length > 0) {\n  events.forEach(function (eventType) {\n    btns.forEach(function (buttons) {\n      buttons.addEventListener(eventType, nextStep);\n    });\n    if (eventType === \"submit\") {\n      btnSubmit.addEventListener(\"submit\", function (event) {\n        stepSucess();\n      });\n    } else {\n      btnSubmit.addEventListener(eventType, stepSucess);\n    }\n  });\n} else {\n  console.error(\"Error: The 'events' array is either empty or not defined.\");\n}\n\n//# sourceURL=webpack:///./js/script.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/arrayLikeToArray.js":
+/*!*********************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/arrayLikeToArray.js ***!
+  \*********************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ _arrayLikeToArray)\n/* harmony export */ });\nfunction _arrayLikeToArray(r, a) {\n  (null == a || a > r.length) && (a = r.length);\n  for (var e = 0, n = Array(a); e < a; e++) n[e] = r[e];\n  return n;\n}\n\n\n//# sourceURL=webpack:///./node_modules/@babel/runtime/helpers/esm/arrayLikeToArray.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/arrayWithoutHoles.js":
+/*!**********************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/arrayWithoutHoles.js ***!
+  \**********************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ _arrayWithoutHoles)\n/* harmony export */ });\n/* harmony import */ var _arrayLikeToArray_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./arrayLikeToArray.js */ \"./node_modules/@babel/runtime/helpers/esm/arrayLikeToArray.js\");\n\nfunction _arrayWithoutHoles(r) {\n  if (Array.isArray(r)) return (0,_arrayLikeToArray_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(r);\n}\n\n\n//# sourceURL=webpack:///./node_modules/@babel/runtime/helpers/esm/arrayWithoutHoles.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/iterableToArray.js":
+/*!********************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/iterableToArray.js ***!
+  \********************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ _iterableToArray)\n/* harmony export */ });\nfunction _iterableToArray(r) {\n  if (\"undefined\" != typeof Symbol && null != r[Symbol.iterator] || null != r[\"@@iterator\"]) return Array.from(r);\n}\n\n\n//# sourceURL=webpack:///./node_modules/@babel/runtime/helpers/esm/iterableToArray.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/nonIterableSpread.js":
+/*!**********************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/nonIterableSpread.js ***!
+  \**********************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ _nonIterableSpread)\n/* harmony export */ });\nfunction _nonIterableSpread() {\n  throw new TypeError(\"Invalid attempt to spread non-iterable instance.\\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method.\");\n}\n\n\n//# sourceURL=webpack:///./node_modules/@babel/runtime/helpers/esm/nonIterableSpread.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/toConsumableArray.js":
+/*!**********************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/toConsumableArray.js ***!
+  \**********************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ _toConsumableArray)\n/* harmony export */ });\n/* harmony import */ var _arrayWithoutHoles_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./arrayWithoutHoles.js */ \"./node_modules/@babel/runtime/helpers/esm/arrayWithoutHoles.js\");\n/* harmony import */ var _iterableToArray_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./iterableToArray.js */ \"./node_modules/@babel/runtime/helpers/esm/iterableToArray.js\");\n/* harmony import */ var _unsupportedIterableToArray_js__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./unsupportedIterableToArray.js */ \"./node_modules/@babel/runtime/helpers/esm/unsupportedIterableToArray.js\");\n/* harmony import */ var _nonIterableSpread_js__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./nonIterableSpread.js */ \"./node_modules/@babel/runtime/helpers/esm/nonIterableSpread.js\");\n\n\n\n\nfunction _toConsumableArray(r) {\n  return (0,_arrayWithoutHoles_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(r) || (0,_iterableToArray_js__WEBPACK_IMPORTED_MODULE_1__[\"default\"])(r) || (0,_unsupportedIterableToArray_js__WEBPACK_IMPORTED_MODULE_2__[\"default\"])(r) || (0,_nonIterableSpread_js__WEBPACK_IMPORTED_MODULE_3__[\"default\"])();\n}\n\n\n//# sourceURL=webpack:///./node_modules/@babel/runtime/helpers/esm/toConsumableArray.js?");
+
+/***/ }),
+
+/***/ "./node_modules/@babel/runtime/helpers/esm/unsupportedIterableToArray.js":
+/*!*******************************************************************************!*\
+  !*** ./node_modules/@babel/runtime/helpers/esm/unsupportedIterableToArray.js ***!
+  \*******************************************************************************/
+/***/ ((__unused_webpack___webpack_module__, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ _unsupportedIterableToArray)\n/* harmony export */ });\n/* harmony import */ var _arrayLikeToArray_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./arrayLikeToArray.js */ \"./node_modules/@babel/runtime/helpers/esm/arrayLikeToArray.js\");\n\nfunction _unsupportedIterableToArray(r, a) {\n  if (r) {\n    if (\"string\" == typeof r) return (0,_arrayLikeToArray_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(r, a);\n    var t = {}.toString.call(r).slice(8, -1);\n    return \"Object\" === t && r.constructor && (t = r.constructor.name), \"Map\" === t || \"Set\" === t ? Array.from(r) : \"Arguments\" === t || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(t) ? (0,_arrayLikeToArray_js__WEBPACK_IMPORTED_MODULE_0__[\"default\"])(r, a) : void 0;\n  }\n}\n\n\n//# sourceURL=webpack:///./node_modules/@babel/runtime/helpers/esm/unsupportedIterableToArray.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./js/script.js");
+/******/ 	
+/******/ })()
+;
